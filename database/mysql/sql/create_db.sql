6c4458273be814572b78975c2d4886da4df3cc0b
ryo <>
2023-11-21 14:59:31 +0900

    fix init sql

--- database/mysql/sql/create_db.sql
+++ database/mysql/sql/create_db.sql
@@ -1,9 +1,14 @@
+use mydb ;
+
 -- CreateTable
-CREATE TABLE `Todo` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+CREATE TABLE `todo` (
+    `todo_id` INTEGER NOT NULL AUTO_INCREMENT,
     `todo` VARCHAR(191) NOT NULL,
     `status` VARCHAR(191) NOT NULL,
     `is_delete` BOOLEAN NOT NULL,
 
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`todo_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+INSERT INTO todo (todo,status,is_delete) VALUE ("todo1","todo",false)
