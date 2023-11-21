4beab4ee08c46c4295701ef6bdb1a75c992475b9
ryo <>
2023-11-21 15:00:53 +0900

    fix config

--- frontend/app/next.config.js
+++ frontend/app/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://express-container:3001/api/:path*',
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
